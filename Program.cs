a1ea417322ca54e2464f15fb96be712400876802
Daniel Llanos Meza <dllanos@farmaciasperuanas.pe>
2020-08-12 21:45:47 -0500

    Agregando nueva propiedad LastName

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace demoRevert
     class Program
     {
         public string name { get; set; }
+        public string lastname { get; set; }             
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
