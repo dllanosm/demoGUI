c2aa5afa7e7384aa4ffd9ea19b0c14cfaaff07ec
Daniel Llanos Meza <dllanos@farmaciasperuanas.pe>
2020-08-12 20:29:22 -0500

    Revert "Añadir edad"
    This reverts commit c4286c7c2bf7578c514da45501e3b59f35406c9d.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ namespace demoRevert
 {
     class Program
     {
-        public int edad { get; set; }
-        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
